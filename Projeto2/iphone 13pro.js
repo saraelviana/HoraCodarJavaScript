d332240947b4b72eea5e179ea1228ac3bd406d33
Manasse Sarael <saraelviana@outlook.com>
2025-10-31 18:29:15 -0300

    fim css

---
+++ Projeto2/iphone 13pro.js
@@ -0,0 +1,11 @@
+/*vamos ter que selecionar os elementos que eu quero */
+/*1 - o botao , vamos selecionar todos eles*/
+const buttons = document.querySelectorAll("#image-picker li")
+/*2 - a imagem para trocar ela, aqui e so 1 elemento a ser escolido */
+const image = document.querySelector("#product-image")
+
+/*agora vamos colocar 1 evento para cada um botoes */
+buttons.forEach((btn)=>{
+    /*aqui dentro vamos botar a função */
+    btn.addEventListener("click(';')", ()=>)
+})/*vamos trabalhar o loop , vou chamar de = btn..*/
